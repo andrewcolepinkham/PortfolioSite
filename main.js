3f1d819728ec1e456781878616644da7290b8cc4
andrewcolepinkham <57509695+andrewcolepinkham@users.noreply.github.com>
2020-01-05 21:41:51 -0800

    My First Commit

---
+++ main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+  $('.menu-toggler').on('click', function () {
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
+  });
+
+  $('.top-nav .nav-link').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  });
+
+  $('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate( {
+      scrollTop: $($(this).attr('href')).offset().top - 100
+    }, 2000);
+  });
+
+  $('#up').on('click', function () {
+    $('html, body').animate( {
+      scrollTop: 0
+    }, 2000);
+  });
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1800,
+    once: true
+  });
+
+  $('.fa-facebook-square').on('click', function () {
+    window.open("https://www.facebook.com/andrew.pinkham.9");
+  });
+
+  $('.fa-linkedin').on('click', function () {
+    window.open("https://www.linkedin.com/in/andrew-pinkham-53309a167/");
+  });
+
+  $('.fa-instagram').on('click', function () {
+    window.open("https://www.instagram.com/andrew_pinkham/?hl=en");
+  });
+  $('.fa-github').on('click', function () {
+    window.open("https://github.com/andrewcolepinkham");
+  });
+});
